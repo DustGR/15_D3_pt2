e91571b1528d0ee4bdfb12bed2c23dfce26f5703
Dustin Rice <dustiferous@gmail.com>
2018-12-10 13:17:14 -0700

    testing

--- tabletest.py
+++ tabletest.py
@@ -1,5 +1,25 @@
+import os
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', "")  or "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
+db.create_all()
 
 # reflect an existing database into a new model
 Base = automap_base()
